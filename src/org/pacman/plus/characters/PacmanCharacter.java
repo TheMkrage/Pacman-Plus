32a7ecd02e2d60f765eb3204e90f38212084c920
Matthew Krager <matthew.krager@STU-MKRAGER.student.friarsnet.servitehs.org>
2015-02-11 14:13:59 -0800

    Signed-off-by: Matthew Krager <matthew.krager@STU-MKRAGER.student.friarsnet.servitehs.org>

--- src/org/pacman/plus/characters/PacmanCharacter.java
+++ src/org/pacman/plus/characters/PacmanCharacter.java
@@ -8,24 +8,26 @@ import org.pacman.plus.main.GameFrame;
 
 public class PacmanCharacter extends GameObject {
 
+	private int lastDirection = 0;
 	public PacmanCharacter(int x, int y, String name) {
 		super(x, y, name);
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	public PacmanCharacter(String name) {
-		super(name); 
+		super(name);
 	}
 
 	public boolean isCollidingWithWall() {
-		if(WallCollisionDetector.isBoundsTouchingWall(getBounds())) {
+		if (WallCollisionDetector.isBoundsTouchingWall(getBounds())) {
 			return true;
 		}
 		return false;
 	}
+
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		if(isCollidingWithWall()) {
+		if (isCollidingWithWall()) {
 			stop();
 		}
 	}
