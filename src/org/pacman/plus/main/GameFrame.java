02293124b9ca6d0fea28fbc1b5577380e7631c94
Matthew Krager <Matthew.Krager@STU-MKRAGER.student.friarsnet.servitehs.org>
2015-02-12 11:03:29 -0800

    fixed transperancy

--- src/org/pacman/plus/main/GameFrame.java
+++ src/org/pacman/plus/main/GameFrame.java
@@ -10,38 +10,37 @@ import org.pacman.plus.gameobject.GameObject;
 import org.pacman.plus.gameobject.course.Wall;
 import org.pacman.plus.keylisteners.BasicKeyListener;
 
-public class GameFrame extends JFrame{
+public class GameFrame extends JFrame {
 
 	public GameFrame() {
-		//set up JFrame
+		// set up JFrame
 		super();
 		setTitle("Pacman-Plus");
 		setSize(800, 700);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
-		
+
 		add(MainPacmanCharacter.getInstance());
-		
+
 		GameObject Background = new GameObject(200, 0, "GameBackground.png");
 		add(Background);
-		
-		
+
 		addKeyListener(new BasicKeyListener());
-		
-		//leaves 200 - 800 for game
+
+		// leaves 200 - 800 for game
 		Wall barrier = new Wall(186, 0, 15, 665);
 		add(barrier);
 		gameLoop();
 	}
-	
+
 	private void gameLoop() {
-		while(true) {
+		while (true) {
 			repaint();
 		}
 	}
-	
+
 	public void paint(Graphics g) {
 		super.paintComponents(g);
+		//.drawImage(MainPacmanCharacter.getInstance().getImage().getImage(), 0, 0, null);
 	}
 }
